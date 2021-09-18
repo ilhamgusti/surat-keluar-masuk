df826836b5721a1b944728a02a0f9976188899e3
irwan_maulana <irwan@dartmedia.biz>
2021-09-18 13:53:30 +0700

    changee titile

--- resources/views/layouts/authLayout.blade.php
+++ resources/views/layouts/authLayout.blade.php
@@ -5,7 +5,7 @@
         <meta charset="utf-8" />
         <title>@yield('title')</title>
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-        <meta content="Premium Multipurpose Admin & Dashboard Template" name="description" />
+        <meta content="Kementrian Agama Republik Indonesia" name="description" />
         <meta content="" name="author" />
         <meta http-equiv="X-UA-Compatible" content="IE=edge" />
 
@@ -34,11 +34,11 @@
         <script src="{{ URL::asset('assets/js/metismenu.min.js') }}"></script>
         <script src="{{ URL::asset('assets/js/waves.js') }}"></script>
         <script src="{{ URL::asset('assets/js/feather.min.js') }}"></script>
-        <script src="{{ URL::asset('assets/js/jquery.slimscroll.min.js') }}"></script>        
+        <script src="{{ URL::asset('assets/js/jquery.slimscroll.min.js') }}"></script>
 
         <!-- App js -->
         <script src="{{ URL::asset('assets/js/app.js') }}"></script>
-        
+
     </body>
 
 </html>
